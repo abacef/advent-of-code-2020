ab19208f2972597c228b27f2ff6f1a985b78b48b
Mark Nash <mdn4993@rit.edu>
2020-12-09 14:41:20 -0500

    Finished day 2 task 2

--- src/aoc2/mod.rs
+++ src/aoc2/mod.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::ops::Index;
 
 const INPUT: &str = "/home/bacef/IdeaProjects/untitled60/src/aoc2/aoc2.txt";
 
@@ -9,7 +10,7 @@ pub struct Password {
     password: String
 }
 
-pub fn solve1() -> usize {
+fn read_input() -> Vec<Password> {
     fs::read_to_string(INPUT).unwrap().lines()
         .map(|x| x.split_whitespace())
         .map(|mut x| {
@@ -20,7 +21,11 @@ pub fn solve1() -> usize {
                 character: x.next().unwrap().as_bytes()[0],
                 password: x.next().unwrap().to_string()
             }
-        })
+        }).collect()
+}
+
+pub fn solve1() -> usize {
+    read_input().iter()
         .map(|x| {
             let mut count = 0;
             for item in x.password.as_bytes() {
@@ -29,6 +34,23 @@ pub fn solve1() -> usize {
                 }
             }
             count >= x.low_number && count <= x.high_number
+        })
+        .filter(|x| *x == true)
+        .count()
+}
+
+pub fn solve2() -> usize {
+    read_input().iter()
+        .map(|x| {
+            let pass_bytes = x.password.as_bytes();
+            let low_char = pass_bytes[(x.low_number - 1) as usize];
+            let high_char = pass_bytes[(x.high_number - 1) as usize];
+
+            if (low_char == x.character) ^ (high_char == x.character) {
+                true
+            } else {
+                false
+            }
         })
         .filter(|x| *x == true)
         .count()
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use crate::aoc2::solve1;
+use crate::aoc2::{solve1, solve2};
 
 mod aoc2;
 
 fn main() {
-    let answer = solve1();
-    println!("answer: {}", answer);
+    let answer1 = solve1();
+    println!("answer 1: {}", answer1);
+
+    let answer2 = solve2();
+    println!("answer 2: {}", answer2)
 }
 
 
