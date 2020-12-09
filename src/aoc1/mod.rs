a05250f4ed94f0dbedac37a9b624bc916ba313b1
Mark Nash <mdn4993@rit.edu>
2020-12-09 02:12:05 -0500

    Optimized part 2

--- src/aoc1/mod.rs
+++ src/aoc1/mod.rs
@@ -19,17 +19,24 @@ impl Solver {
         }
     }
 
-    /// Bin each number based on its last digit.
-    /// If one of the numbers ends in zero, the other does as well. If it is a 9, the other is a 1
-    /// and so on.
-    pub fn solve1(&self) -> Option<u32> {
+    fn build_bins(&self) -> Vec<Vec<u32>> {
         let mut bins: Vec<Vec<u32>> = Vec::with_capacity(10);
         for _ in 0 .. 10 {
             bins.push(Vec::new());
         }
+
         for i in 0 .. self.data.len() {
             bins[(&self.data[i] % 10) as usize].push(self.data[i]);
         }
+
+        bins
+    }
+
+    /// Bin each number based on its last digit.
+    /// If one of the numbers ends in zero, the other does as well. If it is a 9, the other is a 1
+    /// and so on.
+    pub fn solve1(&self) -> Option<u32> {
+        let bins: Vec<Vec<u32>> = self.build_bins();
 
         for i in 0 .. 6 {
             let op_i = (10 - i) % 10;
@@ -50,30 +57,58 @@ impl Solver {
         None
     }
 
+    pub fn solve2(&self) -> Option<u32> {
+        let bins: Vec<Vec<u32>> = self.build_bins();
+
+        for i in 0 .. self.data.len() {
+            for j in 0 .. self.data.len() {
+                if i != j {
+                    let num1_plus_num2 = self.data[i] + self.data[j];
+                    let opt_mod = num1_plus_num2 % 10;
+                    let bin_num = ((10 - opt_mod) % 10) as usize;
+
+                    for k in 0 .. bins[bin_num].len() {
+                        let third_num = bins[bin_num][k];
+                        if num1_plus_num2 + third_num == TARGET {
+                            return Some(&self.data[i] * &self.data[j] * third_num)
+                        } else {
+                            continue;
+                        };
+                    }
+                }
+            }
+        }
+        None
+    }
+
     /// Brute force check every pairwise number
     pub fn old_solve1(&self) -> Option<u32> {
         for i in 0 .. self.data.len() {
             for j in 0 .. self.data.len() {
-                if &self.data[i] + &self.data[j] == TARGET {
-                    return Some(&self.data[i] * &self.data[j])
-                } else {
-                    continue;
-                };
+                if i != j {
+                    if &self.data[i] + &self.data[j] == TARGET {
+                        return Some(&self.data[i] * &self.data[j])
+                    } else {
+                        continue;
+                    };
+                }
             }
         }
         None
     }
 
     /// Brute force check every triwise number
-    pub fn solve2(&self) -> Option<u32> {
+    pub fn old_solve2(&self) -> Option<u32> {
         for i in 0 .. self.data.len() {
             for j in 0 .. self.data.len() {
                 for k in 0 .. self.data.len() {
-                    if &self.data[i] + &self.data[j] + &self.data[k] == TARGET {
-                        return Some(&self.data[i] * &self.data[j] * &self.data[k])
-                    } else {
-                        continue;
-                    };
+                    if i != j && i != k && j != k {
+                        if &self.data[i] + &self.data[j] + &self.data[k] == TARGET {
+                            return Some(&self.data[i] * &self.data[j] * &self.data[k])
+                        } else {
+                            continue;
+                        };
+                    }
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,45 @@ mod aoc1;
 
 const ERROR_MESSAGE: &str = "ERROR! Not found!";
 
+fn display_message(val: Option<u32>) -> String {
+    match val {
+        Some(val) => val.to_string(),
+        None => ERROR_MESSAGE.to_string()
+    }
+}
+
 fn main() {
     let solver = aoc1::Solver::new();
 
-    let old_now = Instant::now();
-    let _ = solver.old_solve1();
-    println!("old part 1 time: {}", old_now.elapsed().as_micros());
+    let old_now1 = Instant::now();
+    let old1 = solver.old_solve1();
+    println!("old part 1 time: {}", old_now1.elapsed().as_micros());
 
-    let new_now = Instant::now();
-    let res1 = solver.solve1();
-    println!("new part 1 time: {}", new_now.elapsed().as_micros());
+    let message1_old = display_message(old1);
+    println!("old part 1 answer: {}", message1_old);
 
-    let message1 = match res1 {
-        Some(val) => val.to_string(),
-        None => ERROR_MESSAGE.to_string()
-    };
-    println!("Part 1 answer: {}", message1);
 
-    let now2 = Instant::now();
-    let res2 = solver.solve2();
-    println!("part 2 time: {}", now2.elapsed().as_micros());
+    let new_now1 = Instant::now();
+    let new1 = solver.solve1();
+    println!("new part 1 time: {}", new_now1.elapsed().as_micros());
 
-    let message2 = match res2 {
-        Some(val) => val.to_string(),
-        None => ERROR_MESSAGE.to_string()
-    };
-    println!("Part 2 answer: {}", message2);
+    let message1_new = display_message(new1);
+    println!("old part 1 answer: {}", message1_new);
+
+
+
+    let old_now2 = Instant::now();
+    let old2 = solver.old_solve2();
+    println!("old part 2 time: {}", old_now2.elapsed().as_micros());
+
+    let message2_new = display_message(old2);
+    println!("old part 2 answer: {}", message2_new);
+
+
+    let new_now2 = Instant::now();
+    let new2 = solver.solve2();
+    println!("new part 2 time: {}", new_now2.elapsed().as_micros());
+
+    let message2_new = display_message(new2);
+    println!("new part 2 answer: {}", message2_new);
 }
